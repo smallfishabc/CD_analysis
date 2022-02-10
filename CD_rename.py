4555c36576c7df7c0b3ed1baf4c025605bd0a040
Feng <fyu9@ucmerced.edu>
2022-02-09 16:21:45 -0800

    Created a function that covert xlsx data to file name which will be used to automatically identify and load CD files

--- CD_rename.py
+++ CD_rename.py
@@ -10,6 +10,7 @@ Rename CD measurement base on
 
 import os
 import pandas as pd
+import numpy as np
 def list_txt_file():   
     list_name=[]
     # List all the files in the current working directory (Similar to DIR command in DOS)
@@ -20,7 +21,29 @@ def list_txt_file():
         # Attach txt file name into the list name
             list_name.append(n.replace('.txt',''))
     return list_name
+def excel_name_edit(old_name):
+    front=old_name.replace('.xlsx','')
+    new_name=front+'_written'+'.xlsx'
+    return new_name
+def write_file_name_csv(dataframe,new_excel_name):
+    for i in range(len(dataframe)):
+        print(dataframe.loc[i,'Tube_number'],type(dataframe.loc[i,'Tube_number']),not dataframe.loc[i,'Tube_number'])
+        if np.isnan(dataframe.loc[i,'Tube_number']):
+            break
+            # Input the component of the file name from xlsx file
+            # For different experiments, the column name maybe different
+            # But the structure will stay the same
+        Solute_concentration=dataframe.loc[i,'Solute_Concentration_Experiment']
+        Solute_type=dataframe.loc[i,'Solute_type']
+        tag=dataframe.loc[i,'tag']
+        time=dataframe.loc[i,'Time_experiment']
+        file_name='_'.join((str(Solute_concentration),str(Solute_type),str(tag),str(time)))
+        dataframe.loc[i,'File_name']=file_name
+        dataframe.loc[i,'Buffer_file']=file_name+'_buffer'
+    dataframe.to_excel(new_excel_name,index=False)
 #def get_CD_file_list():
-    
-test=pd.read_csv('evaporation_experiment_D_0127.csv')
+excel_name='evaporation_0206_CASHD.xlsx'
+test=pd.read_excel(excel_name)
 a=list_txt_file()
+new_excel_name=excel_name_edit(excel_name)
+write_file_name_csv(test,new_excel_name)
